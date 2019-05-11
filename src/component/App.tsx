fcd22b14975aaee51ac8aae405c353fb5685d03b
kimbeomgyu <qjarb1994@gmail.com>
2019-05-11 23:14:17 +0900

    app.tsx 구조생성

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,9 +1,61 @@
 import * as React from 'react';
 import '../assets/css/app.css';
+import good from '../assets/icon/icon-good.png';
+import mist from '../assets/icon/icon-mist.png';
+import logo from '../assets/logo.png';
 
 class App extends React.Component {
   public render() {
-    return <div>hello world</div>;
+    return (
+      <div className="App">
+        <header>
+          <h1>서울</h1>
+          <img src={mist} />
+          <h2>11.25℃</h2>
+          <h2>안개</h2>
+        </header>
+        <section>
+          <h5>오늘 4월 2일 화요일</h5>
+          <div>
+            <tr>
+              <th>
+                <p>미세먼지</p>
+                <div>
+                  <img src={good} />
+                  <h2>34</h2>
+                </div>
+              </th>
+              <th>
+                <p>초미세먼지</p>
+                <div>
+                  <img src={good} />
+                  <h2>18</h2>
+                </div>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <p>오존</p>
+                <div>
+                  <img src={good} />
+                  <h2>0.051</h2>
+                </div>
+              </th>
+              <th>
+                <p>이산화질소</p>
+                <div>
+                  <img src={good} />
+                  <h2>0.011</h2>
+                </div>
+              </th>
+            </tr>
+          </div>
+        </section>
+        <footer>
+          <img src={logo} />
+        </footer>
+      </div>
+    );
   }
 }
 
