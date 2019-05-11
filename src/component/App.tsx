bfaa0f52dc1dc8d7f326df9b2a6edf97065c9c36
kimbeomgyu <qjarb1994@gmail.com>
2019-05-12 00:29:00 +0900

    scss 적용

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import '../assets/css/app.css';
 import good from '../assets/icon/icon-good.png';
 import mist from '../assets/icon/icon-mist.png';
 import logo from '../assets/logo.png';
+import '../assets/scss/app.css';
 
 class App extends React.Component {
   public render() {
@@ -16,40 +16,42 @@ class App extends React.Component {
         </header>
         <section>
           <h5>오늘 4월 2일 화요일</h5>
-          <div>
-            <tr>
-              <th>
-                <p>미세먼지</p>
-                <div>
-                  <img src={good} />
-                  <h2>34</h2>
-                </div>
-              </th>
-              <th>
-                <p>초미세먼지</p>
-                <div>
-                  <img src={good} />
-                  <h2>18</h2>
-                </div>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <p>오존</p>
-                <div>
-                  <img src={good} />
-                  <h2>0.051</h2>
-                </div>
-              </th>
-              <th>
-                <p>이산화질소</p>
-                <div>
-                  <img src={good} />
-                  <h2>0.011</h2>
-                </div>
-              </th>
-            </tr>
-          </div>
+          <table>
+            <thead>
+              <tr>
+                <td>
+                  <p>미세먼지</p>
+                  <div>
+                    <img src={good} />
+                    <h2>34</h2>
+                  </div>
+                </td>
+                <td>
+                  <p>초미세먼지</p>
+                  <div>
+                    <img src={good} />
+                    <h2>18</h2>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>오존</p>
+                  <div>
+                    <img src={good} />
+                    <h2>0.051</h2>
+                  </div>
+                </td>
+                <td>
+                  <p>이산화질소</p>
+                  <div>
+                    <img src={good} />
+                    <h2>0.011</h2>
+                  </div>
+                </td>
+              </tr>
+            </thead>
+          </table>
         </section>
         <footer>
           <img src={logo} />
