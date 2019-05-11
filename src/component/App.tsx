70c09531c138d238db7ef297f3ab6d96a767f8e6
kimbeomgyu <qjarb1994@gmail.com>
2019-05-11 18:45:16 +0900

    intial commit

---
+++ src/component/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import '../assets/css/app.css';
+
+class App extends React.Component {
+  public render() {
+    return <div>hello world</div>;
+  }
+}
+
+export default App;
