b5bf60a299b479481db3bc7b826457d613fb778a
kimbeomgyu <qjarb1994@gmail.com>
2019-05-15 07:45:43 +0900

    class component -> stateless component Weather_head, Weather_main

--- src/containers/Weather_header.tsx
+++ src/containers/Weather_header.tsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { StatelessComponent as Component } from 'react';
 import { connect } from 'react-redux';
 import WeatherIcon from '../component/Weather_icon';
 import { IWeatherToProps } from '../Type';
 
-class WeatherHeader extends Component<IWeatherToProps> {
-  public render() {
-    const [{ main, weather }] = this.props.weather;
-    const { temp } = main;
-    const [{ description, icon }] = weather;
-
-    return (
-      <header>
-        <h1>서울</h1>
-        <WeatherIcon icon={icon} />
-        <h2>{(temp - 273.15).toFixed(1)}℃</h2>
-        <h2>{description}</h2>
-      </header>
-    );
-  }
-}
+const WeatherHeader: Component<IWeatherToProps> = ({
+  weather: [
+    {
+      main: { temp },
+      weather: [{ description, icon }]
+    }
+  ]
+}) => {
+  return (
+    <header>
+      <h1>서울</h1>
+      <WeatherIcon icon={icon} />
+      <h2>{(temp - 273.15).toFixed(1)}℃</h2>
+      <h2>{description}</h2>
+    </header>
+  );
+};
 
 function mapStateToProps({ weather }: IWeatherToProps) {
   return { weather };
--- src/containers/Weather_main.tsx
+++ src/containers/Weather_main.tsx
@@ -1,34 +1,36 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { StatelessComponent as Component } from 'react';
 import { connect } from 'react-redux';
 import TableValue from '../component/Table';
 import { IDataType, IWeatherToProps } from '../Type';
 
-class WeatherMain extends Component<IWeatherToProps> {
-  public render() {
-    const [{ ListAvgOfSeoulAirQualityService }] = this.props.air;
-    const { row } = ListAvgOfSeoulAirQualityService;
-    const [{ PM10, PM25, OZONE, NITROGEN }] = row;
+const WeatherMain: Component<IWeatherToProps> = ({
+  air: [
+    {
+      ListAvgOfSeoulAirQualityService: {
+        row: [{ PM10, PM25, OZONE, NITROGEN }]
+      }
+    }
+  ]
+}) => {
+  const data = [
+    { name1: '미세먼지', name2: '초미세먼지', value1: PM10, value2: PM25 },
+    { name1: '오존', name2: '이산화질소', value1: OZONE, value2: NITROGEN }
+  ];
 
-    const data = [
-      { name1: '미세먼지', name2: '초미세먼지', value1: PM10, value2: PM25 },
-      { name1: '오존', name2: '이산화질소', value1: OZONE, value2: NITROGEN }
-    ];
-
-    return (
-      <table>
-        <thead>
-          {data.map(({ name1, name2, value1, value2 }: IDataType) => (
-            <tr key={name1 + name2}>
-              <TableValue name={name1} value={value1} />
-              <TableValue name={name2} value={value2} />
-            </tr>
-          ))}
-        </thead>
-      </table>
-    );
-  }
-}
+  return (
+    <table>
+      <thead>
+        {data.map(({ name1, name2, value1, value2 }: IDataType) => (
+          <tr key={name1 + name2}>
+            <TableValue name={name1} value={value1} />
+            <TableValue name={name2} value={value2} />
+          </tr>
+        ))}
+      </thead>
+    </table>
+  );
+};
 
 function mapStateToProps({ air }: IWeatherToProps) {
   return { air };
